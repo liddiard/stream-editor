b0a47af1fc79f489bea4c1ebf739cd68c36b74d7
omniaura <omniaura5@gmail.com>
2015-07-18 03:19:04 -0400

    added url encoding and decoding utils

---
+++ stream_editor/static/src/utils.js
@@ -0,0 +1,55 @@
+var Utils = function() {
+
+  var encodeUrl = function(operations) {
+
+    var urlComponents = {};
+    operations.forEach(function(operation, index){
+      urlComponents['command-'+index] = operation.command;
+      urlComponents['args-'+index] = operation.args;
+    });
+
+    var encodedPairs = [];
+    for (var key in urlComponents) {
+      if (urlComponents.hasOwnProperty(key)) {
+        encodedPairs.push(window.encodeURIComponent(key) + '=' +
+                          window.encodeURIComponent(urlComponents[key]));
+      }
+    }
+
+    var querystring = "?" + encodedPairs.join('&');
+    return querystring;
+  };
+
+
+  var decodeUrl = function(querystring) {
+
+    var encodedPairs = querystring.slice(1).split('&');
+
+    var urlComponents = {};
+    encodedPairs.forEach(function(pair, index){
+      components = pair.split('=');
+      urlComponents[window.decodeURIComponent(components[0])] = window.decodeURIComponent(components[1]);
+    });
+
+    var operations = [];
+    var index = 0;
+    while(1) {
+      if (urlComponents.hasOwnProperty('command-'+index) &&
+          urlComponents.hasOwnProperty('args-'+index)) {
+        operations.push({
+          command: urlComponents['command-'+index],
+          args: urlComponents['args-'+index],
+          error: ''
+        });
+        index++;
+      }
+      else break;
+    }
+
+    return operations;
+  };
+
+}
+
+
+module.exports = Utils;
