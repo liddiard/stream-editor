9fd0f3ce9f6cc52b8f298caeb0965d86be97d491
omniaura <omniaura5@gmail.com>
2015-08-08 12:59:59 -0700

    added some command resources

--- stream_editor/settings.py
+++ stream_editor/settings.py
@@ -5,72 +5,103 @@ SUPPORTED_COMMANDS = [
     {
         'name': 'sed',
         'description': 'multipurpose stream editor',
-        'resources': []
+        'resources': [
+            'http://man.cx/sed',
+            'https://www.digitalocean.com/community/tutorials/the-basics-of-using-the-sed-stream-editor-to-manipulate-text-in-linux',
+            'http://www.panix.com/~elflord/unix/sed.html',
+            'http://www.grymoire.com/Unix/Sed.html'
+        ]
     },
     {
         'name': 'grep',
         'description': 'search text for a pattern',
-        'resources': []
+        'resources': [
+            'http://man.cx/grep',
+        ]
     },
     {
         'name': 'awk',
         'description': 'pattern scanning and processing language',
-        'resources': []
+        'resources': [
+            'http://man.cx/awk',
+        ]
     },
     {
         'name': 'tr',
         'description': 'translate characters',
-        'resources': []
+        'resources': [
+            'http://man.cx/tr',
+        ]
     },
     {
         'name': 'cut',
         'description': 'cut out selected fields of each line of a file',
-        'resources': []
+        'resources': [
+            'http://man.cx/cut',
+        ]
     },
     {
         'name': 'sort',
         'description': 'sort, merge, and sequence-check text',
-        'resources': []
+        'resources': [
+            'http://man.cx/sort',
+        ]
     },
     {
         'name': 'tsort',
         'description': 'topological sort',
-        'resources': []
+        'resources': [
+            'http://man.cx/tsort',
+        ]
     },
     {
         'name': 'uniq',
         'description': 'report or filter out repeated lines',
-        'resources': []
+        'resources': [
+            'http://man.cx/uniq',
+        ]
     },
     {
         'name': 'expand',
         'description': 'convert tabs to spaces',
-        'resources': []
+        'resources': [
+            'http://man.cx/expand',
+        ]
     },
     {
         'name': 'unexpand',
         'description': 'convert spaces to tabs',
-        'resources': []
+        'resources': [
+            'http://man.cx/unexpand',
+        ]
     },
     {
         'name': 'fold',
         'description': 'filter for folding lines',
-        'resources': []
+        'resources': [
+            'http://man.cx/fold',
+        ]
     },
     {
         'name': 'head',
         'description': 'copy the first part of text',
-        'resources': []
+        'resources': [
+            'http://man.cx/head',
+        ]
     },
     {
         'name': 'tail',
         'description': 'copy the last part of text',
-        'resources': []
+        'resources': [
+            'http://man.cx/tail',
+        ]
     },
     {
         'name': 'cat',
         'description': 'concatenate, format, and print text',
-        'resources': []
+        'resources': [
+            'http://man.cx/cat',
+        ]
     },
 ]
 
