01d34d402a383eb92e7aa9823831aa1782f35253
omniaura <omniaura5@gmail.com>
2017-03-26 16:53:42 -0700

    set debug based on env var

--- runserver.py
+++ runserver.py
@@ -1,4 +1,9 @@
+import os
+
 from stream_editor import app
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # set debug mode OFF if a `PROD` environment variable is not set 
+    app.config['DEBUG'] = not os.environ.get('PROD') 
+    app.run()
