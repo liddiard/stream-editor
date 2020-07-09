acb1ad7863e3a0e9cd2205699ac020a22c49c814
Harrison Liddiard <hliddiard@paypal.com>
2020-07-08 19:10:43 -0700

    add ratelimiter

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,30 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
 app = Flask(__name__)
+
+# https://flask-cors.readthedocs.io/en/latest/
 CORS(app, origins=[
   'http://localhost:3000'
 ])
 
+# https://flask-limiter.readthedocs.io/en/stable/
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["600 per day", "200 per hour"]
+)
+
+@app.errorhandler(429)
+def ratelimit_handler(e):
+    return jsonify(error={
+      'message': f"Ratelimit request rule exceeded: {e.description}",
+      'index': 0
+    }), 429
 
 import server.views
