8faf152a64a3e732dfcf80f27f3962d98454b709
Harrison Liddiard <hliddiard@paypal.com>
2020-07-15 21:58:14 -0700

    add jail chroot to execute_command

--- runserver.py
+++ runserver.py
@@ -4,6 +4,10 @@ from server import app
 
 
 if __name__ == '__main__':
-    # set debug mode OFF if a `PROD` environment variable is not set 
-    app.config['DEBUG'] = not os.environ.get('PROD') 
+    # Only turn debug mode on if the enviornment variable is set.
+    # ⚠️ WARNING: Running in debug mode is UNSAFE for any amount of time on a
+    # publicly accessible server as it gives anyone on the internet
+    # unrestricted filesystem access to whatever server it's running on, which
+    # may compromise the server, along with any other servers/services that
+    # use credentials stored on it. DON'T DO IT!
     app.run()
