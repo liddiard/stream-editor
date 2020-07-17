93b96d9c2a1d3303465171e7eef0bce1d87fe674
Harrison Liddiard <hliddiard@paypal.com>
2020-07-16 19:13:24 -0700

    update jail script to actually work

--- create_jail.sh
+++ create_jail.sh
@@ -2,23 +2,25 @@
 # assumes you already have jailkit installed by following the instructions at:
 # http://www.mattheakis.com/blog/view.php?name=setting_up_a_jail_to_safely_execute_code
 
-SUPPORTED_COMMANDS = ( awk cat cut expand fold grep head sed sort tail tr tsort unexpand uniq wc )
-JAILED_USER_USERNAME = 'untrusted_internet_user'
+SUPPORTED_COMMANDS=( awk cat cut expand fold grep head sed sort tail tr tsort unexpand uniq wc )
+JAILED_USER_USERNAME='untrusted_internet_user'
+JAIL_PATH=~/jail
 
-# create the jail directory
-mkdir -p ~/jail
-j=~/jail
+echo 'Creating jail directory...'
+mkdir -p JAIL_PATH
 
-# initialize the jail
-jk_init -j $j jk_lsh
+echo 'Initializing the jail...'
+jk_init -j $JAIL_PATH jk_lsh
 
-# create a jailed user
+echo 'Creating jailed user...'
 useradd -m $JAILED_USER_USERNAME
-jk_jailuser -j $j $JAILED_USER_USERNAME
+jk_jailuser -j $JAIL_PATH $JAILED_USER_USERNAME
 
-# copy necessary commands to the jail
-for command in SUPPORTED_COMMANDS
+echo 'Copying supported commands to the jail...'
+for command in "${SUPPORTED_COMMANDS[@]}"
 do
     command_path=$(which $command)
-    jk_cp -j $j $command_path
+    jk_cp -j $JAIL_PATH $command_path
 done
+
+echo 'Done!'
