f4b3c361c93216332610e9e2275f33cfede39de0
Harrison Liddiard <hliddiard@paypal.com>
2020-07-31 18:58:51 -0700

    fix number of panes sync w/ querystring operations

--- client/src/context/initialState.js
+++ client/src/context/initialState.js
@@ -7,11 +7,14 @@ import {
 import {
   getOperationsFromQuerystring,
   getJSONFromLocalStorage,
-  getJSONFromSessionStorage
+  getJSONFromSessionStorage,
+  uuid
 } from '../utils'
 
 
 const inputFromSessionStorage = sessionStorage.getItem('input')
+const panesFromSessionStorage = getJSONFromSessionStorage('panes')
+const querystringOperations = getOperationsFromQuerystring()
 
 export default {
   loading: false,
@@ -20,13 +23,21 @@ export default {
     inputFromSessionStorage :
     INITIAL_INPUT,
   operations: inputFromSessionStorage !== null ? // user's commands + arguments
-    (getOperationsFromQuerystring() || [DEFAULT_OPERATION]) :
+    (querystringOperations || [DEFAULT_OPERATION]) :
     [INITIAL_OPERATION],
   apiInput: '', // input from which the outputs were generated.
                 // needed for diff b/c there's a delay between input
                 // and receiving API resoponse.
   outputs: [],  // list of outputs after each operation is performed
-  panes: getJSONFromSessionStorage('panes') || DEFAULT_PANES,
+  // input and output panes
+  // if there are panes in session storage, increase the number to accommodate
+  // the number of operations from the querystring, if applicable, otherwise
+  // show the default panes
+  panes: panesFromSessionStorage ? panesFromSessionStorage.concat(
+    Array(querystringOperations.length - panesFromSessionStorage.length)
+    .fill(null)
+    .map(() => ({ id: uuid() }))
+  ) : DEFAULT_PANES,
   errors: {
     upload: {
       message: null
